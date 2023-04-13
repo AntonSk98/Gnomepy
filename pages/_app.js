10df7faf8a460487fa9a1e501f17dfe05002ddee
AntonSk98 <antonskripin@gmail.com>
2023-04-13 18:41:39 +0200

    Updated main page

--- pages/_app.js
+++ pages/_app.js
@@ -53,30 +53,14 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Ticket management system selfhosted open source" />
-        <meta name="keywords" content="Keywords" />
-        <title>Peppermint</title>
+        <meta name="description" content="Gnomepy" />
+        <meta name="keywords" content="Gnomepy" />
+        <title>Gnomepy</title>
 
-        {/* <link rel="manifest" href="/manifest.json" /> */}
-        
         <link
           href="/favicon/favicon.ico"
           rel="icon"
         />
-        <link
-          href="/favicon/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/favicon/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
       </Head>
       <SessionProvider session={session}>
         <QueryClientProvider client={queryClient}>
